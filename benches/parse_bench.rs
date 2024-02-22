6ee8be7761c1c9ab9eaef0298e135740c404ce7a
Magnus Hovland Hoff <maghoff@gmail.com>
2024-02-22 10:24:51 +0100

    Add benchmark for parsing to Id30Parse

--- benches/parse_bench.rs
+++ benches/parse_bench.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
-use id30::Id30;
+use id30::{Id30, Id30Parse};
 
-fn parse(b: &mut Bencher) {
+fn parse_id30(b: &mut Bencher) {
     let candidates: Vec<_> = (0..100)
         .map(|_| rand08::random::<Id30>().to_string())
         .collect();
@@ -9,6 +9,19 @@ fn parse(b: &mut Bencher) {
     b.iter(|| {
         for candidate in &candidates {
             let id30: Id30 = candidate.parse().unwrap();
+            black_box(id30);
+        }
+    });
+}
+
+fn parse_id30parse(b: &mut Bencher) {
+    let candidates: Vec<_> = (0..100)
+        .map(|_| rand08::random::<Id30>().to_string())
+        .collect();
+
+    b.iter(|| {
+        for candidate in &candidates {
+            let id30: Id30Parse = candidate.parse().unwrap();
             black_box(id30);
         }
     });
@@ -29,7 +42,8 @@ fn parse_u32(b: &mut Bencher) {
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse_u32", parse_u32);
-    c.bench_function("parse", parse);
+    c.bench_function("parse_id30", parse_id30);
+    c.bench_function("parse_id30parse", parse_id30parse);
 }
 
 criterion_group!(benches, criterion_benchmark);
